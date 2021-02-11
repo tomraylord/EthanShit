0fdc6e62bd86609a1b40282f3e6350034025ca23
tom <52078213+lordtoing@users.noreply.github.com>
2021-02-11 21:36:43 +0000

    Update Settings.java

--- src/cloud/timothy/genericplugin/settings/Settings.java
+++ src/cloud/timothy/genericplugin/settings/Settings.java
@@ -12,7 +12,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -44,9 +43,9 @@ public class Settings implements CommandExecutor, Listener
         {
             return;
         }
+        Player player = (Player) event.getWhoClicked();
         if (Objects.requireNonNull(Objects.requireNonNull(event.getCurrentItem()).getItemMeta()).getDisplayName().equals(ChatColor.YELLOW + "Toggle Chat Filter"))
         {
-            Player player = (Player) event.getWhoClicked();
             if (FilterEnabled.filterEnabled.contains(event.getWhoClicked().getUniqueId().toString()))
             {
                 FilterEnabled.filterEnabled.remove(event.getWhoClicked().getUniqueId().toString());
@@ -57,18 +56,26 @@ public class Settings implements CommandExecutor, Listener
             }
             openSettingsGUI(player);
         }
+        if (Objects.requireNonNull(Objects.requireNonNull(event.getCurrentItem()).getItemMeta()).getDisplayName().equals(ChatColor.YELLOW + "Toggle Scoreboard"))
+        {
+            player.chat("/sgs");
+        }
+        if (Objects.requireNonNull(Objects.requireNonNull(event.getCurrentItem()).getItemMeta()).getDisplayName().equals(ChatColor.RED + "Click to close."))
+        {
+            player.closeInventory();
+        }
         event.setCancelled(true);
     }
 
     public void openSettingsGUI(Player player)
     {
-        final Inventory inventory = Bukkit.createInventory((InventoryHolder) null, 9, ChatColor.YELLOW + "Settings");
+        final Inventory inventory = Bukkit.createInventory(null, 9, ChatColor.YELLOW + "Settings");
 
         final ItemStack chatFilterSetting = new ItemStack(Material.BOOK);
         final ItemMeta chatFilterSettingMeta = chatFilterSetting.getItemMeta();
         assert chatFilterSettingMeta != null;
         chatFilterSettingMeta.setDisplayName(ChatColor.YELLOW + "Toggle Chat Filter");
-        final List<String> chatFilterLore = new ArrayList<String>();
+        final List<String> chatFilterLore = new ArrayList<>();
         chatFilterLore.add("");
         chatFilterLore.add(ChatColor.GRAY + "The Chat Filter will remove all");
         chatFilterLore.add(ChatColor.GRAY + "inappropriate messages from the");
@@ -85,9 +92,33 @@ public class Settings implements CommandExecutor, Listener
             chatFilterLore.add(ChatColor.GRAY + " ▶ Enabled");
             chatFilterLore.add(ChatColor.RED + " ▶ Disabled");
         }
+        chatFilterLore.add("");
         chatFilterSettingMeta.setLore(chatFilterLore);
         chatFilterSetting.setItemMeta(chatFilterSettingMeta);
         inventory.setItem(0, chatFilterSetting);
+
+        final ItemStack toggleScoreboard = new ItemStack(Material.PAPER);
+        final ItemMeta toggleScoreboardMeta = toggleScoreboard.getItemMeta();
+        assert toggleScoreboardMeta != null;
+        toggleScoreboardMeta.setDisplayName(ChatColor.YELLOW + "Toggle Scoreboard");
+        final List<String> toggleScoreboardMetaLore = new ArrayList<>();
+        toggleScoreboardMetaLore.add("");
+        toggleScoreboardMetaLore.add(ChatColor.GRAY + "Choose weather you want");
+        toggleScoreboardMetaLore.add(ChatColor.GRAY + "the sidebar to be shown");
+        toggleScoreboardMetaLore.add(ChatColor.GRAY + "or hidden.");
+        toggleScoreboardMetaLore.add("");
+        toggleScoreboardMetaLore.add(ChatColor.GRAY + "Click to toggle.");
+        toggleScoreboardMetaLore.add("");
+        toggleScoreboardMeta.setLore(toggleScoreboardMetaLore);
+        toggleScoreboard.setItemMeta(toggleScoreboardMeta);
+        inventory.setItem(1, toggleScoreboard);
+
+        final ItemStack exit = new ItemStack(Material.BARRIER);
+        final ItemMeta exitMeta = exit.getItemMeta();
+        assert exitMeta != null;
+        exitMeta.setDisplayName(ChatColor.RED + "Click to close.");
+        exit.setItemMeta(exitMeta);
+        inventory.setItem(8, exit);
 
         player.openInventory(inventory);
     }
